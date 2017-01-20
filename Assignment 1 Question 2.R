ddf4c9b6df04d4a3377c79a15c707111c4004aa0
DanRossLi <dan.gordon.li@gmail.com>
2017-01-20 11:18:47 -0500

    Add files via upload

--- Assignment 1 Question 2.R
+++ Assignment 1 Question 2.R
@@ -14,6 +14,9 @@ cars <- read.csv(file.loc.root)
 n=nrow(cars)
 attach(cars)
 
+###############################
+#Q2.2 - Q2.4 
+###############################
 ## Part 2
 ntrain = round(n*.75)
 
@@ -22,10 +25,12 @@ train=cars[tr,] #training data
 test=cars[-tr,] #test data
 
 ## Part 3
+options(scipen=999)
 Q2.lm = lm(price~mileage, data = train)
 par(mfrow=c(1,1)) # 1 x 1 Graph
 plot(train$mileage,train$price,pch = 20, col = 'black', #Plots of all data
-     cex.main = 1, cex.lab = 1, cex.axis = 1, main = "Plot of Price and Mileage")
+     cex.main = 1, cex.lab = 1, cex.axis = .75, main = "Plot of Price and Mileage",
+     ylab = "Price", xlab = "Mileage")
 abline(Q2.lm$coef, col = 'red', lwd = 2)
 
 ## Part 4
@@ -39,11 +44,10 @@ n.folds <- 10
 poly <- 5
 i.folds <- sample(rep(1:n.folds, length.out = n))
 as.data.frame(table(i.folds))   ## OUTPUTS THE TABULATION OF samples for checking
-cv <- matrix(NA, nrow = n.folds, ncol = ploy)
+cv <- matrix(NA, nrow = n.folds, ncol = poly)
 
 
 for(p in 1:poly) { ## Goes through polynomial
-
   for(k in 1:n.folds) { ## Goes through folds
     i.test = which (i.folds == k) ##Pulls out index of number
     train = cars[-i.test,]
@@ -71,16 +75,16 @@ lines(1:poly, cv.mean,type="l", col = 'red')
 
 Q2.lm.best = lm(price~poly(mileage,cv.best), data = cars)  ##Use Cars or Training? This will have ramificatiosn above
 
-
-
 ###############################
-#Q2.5 KNN and TREES
+#Q2.5 KNN 
+###############################
+
 
 download.file("https://raw.githubusercontent.com/ChicagoBoothML/HelpR/master/docv.R", "docv.R")
 source("docv.R") #this has docvknn used below
 #do k-fold cross validation, 5 twice, 10 once
 set.seed(99) #always set the seed! 
-kv = 2:100 #these are the k values (k as in kNN) we will try
+kv = c(seq(2, 10, 2),seq(10, 100, 10)) #these are the k values (k as in kNN) we will try
 #docvknn(matrix x, vector y,vector of k values, number of folds),
 #does cross-validation for training data (x,y).
 cv1 = docvknn(matrix(cars$mileage,ncol=1),cars$price,kv,nfold=5)
@@ -103,51 +107,191 @@ cv = (cv1+cv2+cv3)/3 #use average
 kbest = kv[which.min(cv)]
 cat("the best k is: ",kbest,"\n")
 #fit kNN with best k and plot the fit.
-kfbest = kknn(medv~lstat,Boston,data.frame(lstat=sort(Boston$lstat)),
+kfbest = kknn(price~mileage,cars,data.frame(mileage=sort(cars$mileage)),
               k=kbest,kernel = "rectangular")
-plot(Boston$lstat,Boston$medv,cex.lab=1.2)
-lines(sort(Boston$lstat),kfbest$fitted,col="red",lwd=2,cex.lab=2)
-
-
-
-
-
-
+plot(cars$mileage,cars$price,cex.lab=1.2)
+lines(sort(cars$mileage),kfbest$fitted,col="red",lwd=2,cex.lab=2)
 
 ###############################
-
-# fit a tree to boston data just using lstat.
-# first get a big tree by appropriately setting rpart.control
-#
-# see help by typing ?rpart.control for more details
-# you will notice that you can set
-#   - minsplit - which sets the minimum number of observations needed in a node to consider a plit
-#   - cp - splits are considered only if the fit is improved by amount controled by cp
-#
-#
-temp = rpart(medv~lstat, data=Boston, 
+#Q2.5 TREE 
+###############################
+big.tree = rpart(price~mileage, data=cars, 
              control=rpart.control(minsplit=5,  # we want to have at least 5 observations in a node
-                                   cp=0.001,    # we will consider even small improvements in a fit 
-                                   xval=0)      # do not run cross-validation now
-)
-rpart.plot(temp)
-#if the tree is too small, make cp smaller!!
+                                   cp=0.0001,    # we will consider even small improvements in a fit 
+                                   xval=10))
+rpart.plot(big.tree)
 
-# then prune it down to one with 7 leaves
-# again, we control the size of the tree through cp 
-# which is not the most intuitive of measures
-boston.tree = prune(temp, cp=0.01)
-rpart.plot(boston.tree)
+nbig = length(unique(big.tree$where))
+cat('size of big tree: ',nbig,'\n')
 
-## plot data with fit
-boston.fit = predict(boston.tree) #get training fitted values
-plot(Boston$lstat, Boston$medv, cex=.5, pch=16) #plot data
-oo=order(Boston$lstat)
-lines(Boston$lstat[oo],boston.fit[oo],col="red",lwd=3) #step function fit
+# let us look at the cross-validation results
+#
+# the following prints out a table summarizing the output of cross-validation
+# you want to find cp corrsponding to the smallest value of xerror 
+(cptable = printcp(big.tree))
+(bestcp = cptable[ which.min(cptable[,"xerror"]), "CP" ])   # this is the optimal cp parameter
 
-# predict at lstat = 15 and 25.
-preddf = data.frame(lstat=c(15,25))
-yhat = predict(boston.tree,preddf)
-points(preddf$lstat,yhat,col="blue",pch="*",cex=3)
+plotcp(big.tree) # plot results
 
+# show fit from some trees
+oo = order(cars$mileage)
+cpvec = c(.0157,bestcp,.004)
 
+par(mfrow=c(3,2))
+for(i in 1:3) {
+  plot(cars$mileage,cars$price,pch=16,col='blue',cex=.5)
+  ptree = prune(big.tree,cp=cpvec[i])
+  pfit = predict(ptree)
+  lines(cars$mileage[oo],pfit[oo],col='red',lwd=2)
+  title(paste('alpha = ',round(cpvec[i],3)))
+  rpart.plot(ptree)
+}
+
+par(mfrow=c(1,1))
+best.tree = prune(big.tree,cp=bestcp)
+rpart.plot(best.tree)
+
+###############################
+#Q2.5 BEST 
+###############################
+
+plot(cars$mileage,cars$price,pch = 20, col = 'black', #Plots of all data
+     cex.main = 1, cex.lab = 1, cex.axis = .75, main = "Plot of Price and Mileage",
+     ylab = "Price", xlab = "Mileage")
+#Adds Reg Line
+pfit = predict(Q2.lm.best, data = cars$mileage)
+lines(cars$mileage[oo],pfit[oo],col='green',lwd=2) ## Reg
+#Adds KNN Line
+lines(sort(cars$mileage),kfbest$fitted,col="blue",lwd=2,cex.lab=2)
+#Adds Tree Line
+pfit = predict(best.tree)
+lines(cars$mileage[oo],pfit[oo],col='red',lwd=2) ## Tree
+legend("topright",legend=c("Regression","KNN","Tree"),
+       col=c("green","blue","red"),lwd=2,cex=.75)
+
+###############################
+#Q2.6 MILEAGE AND YEAR
+###############################
+
+###############################
+#Q2.6 KNN 
+###############################
+download.file("https://raw.githubusercontent.com/ChicagoBoothML/HelpR/master/docv.R", "docv.R")
+source("docv.R") #this has docvknn used below
+#do k-fold cross validation, 5 twice, 10 once
+set.seed(99) #always set the seed! 
+kv = c(seq(2, 10, 2),seq(10, 100, 10)) #these are the k values (k as in kNN) we will try
+#docvknn(matrix x, vector y,vector of k values, number of folds),
+#does cross-validation for training data (x,y).
+cv1 = docvknn(matrix(c(cars$mileage, cars$year),ncol=2),cars$price,kv,nfold=5)
+cv2 = docvknn(matrix(c(cars$mileage, cars$year),ncol=2),cars$price,kv,nfold=5)
+cv3 = docvknn(matrix(c(cars$mileage, cars$year),ncol=2),cars$price,kv,nfold=10)
+#docvknn returns error sum of squares, want RMSE
+cv1 = sqrt(cv1/length(cars$price))
+cv2 = sqrt(cv2/length(cars$price))
+cv3 = sqrt(cv3/length(cars$price))
+
+#plot
+rgy = range(c(cv1,cv2,cv3))
+plot(log(1/kv),cv1,type="l",col="red",ylim=rgy,lwd=2,cex.lab=2.0, xlab="log(1/k)", ylab="RMSE")
+lines(log(1/kv),cv2,col="blue",lwd=2)
+lines(log(1/kv),cv3,col="green",lwd=2)
+legend("topleft",legend=c("5-fold 1","5-fold 2","10 fold"),
+       col=c("red","blue","green"),lwd=2,cex=1.5)
+#get the min
+cv = (cv1+cv2+cv3)/3 #use average
+kbest = kv[which.min(cv)]
+cat("the best k is: ",kbest,"\n")
+#fit kNN with best k and plot the fit.
+
+## This might need adjusting
+kfbest = kknn(price~mileage+year,cars,data.frame(mileage=sort(cars$mileage)),
+              k=kbest,kernel = "rectangular")
+plot(cars$mileage,cars$price,cex.lab=1.2)
+lines(sort(cars$mileage),kfbest$fitted,col="red",lwd=2,cex.lab=2)
+
+###############################
+#Q2.6 TREE '''Add year into reg eq'
+###############################
+big.tree = rpart(price~mileage+year, data=cars, 
+                 control=rpart.control(minsplit=5,  # we want to have at least 5 observations in a node
+                                       cp=0.0001,    # we will consider even small improvements in a fit 
+                                       xval=10))
+rpart.plot(big.tree)
+
+nbig = length(unique(big.tree$where))
+cat('size of big tree: ',nbig,'\n')
+
+# let us look at the cross-validation results
+#
+# the following prints out a table summarizing the output of cross-validation
+# you want to find cp corrsponding to the smallest value of xerror 
+(cptable = printcp(big.tree))
+(bestcp = cptable[ which.min(cptable[,"xerror"]), "CP" ])   # this is the optimal cp parameter
+
+## Finds improved xerror
+min(cptable[,"xerror"])
+## Min xerror .2734326 vs .08866526
+
+plotcp(big.tree) # plot results
+
+# show fit from some trees
+oo = order(cars$mileage)
+cpvec = c(.0157,bestcp,.004)
+
+par(mfrow=c(3,2))
+for(i in 1:3) {
+  plot(cars$mileage,cars$price,pch=16,col='blue',cex=.5)
+  ptree = prune(big.tree,cp=cpvec[i])
+  pfit = predict(ptree)
+  lines(cars$mileage[oo],pfit[oo],col='red',lwd=2)
+  title(paste('alpha = ',round(cpvec[i],3)))
+  rpart.plot(ptree)
+}
+
+par(mfrow=c(1,1))
+best.tree = prune(big.tree,cp=bestcp)
+rpart.plot(best.tree)
+
+###############################
+#Q2.7 TREE '''Use . to utilize all variables
+###############################
+big.tree = rpart(price~., data=cars, 
+                 control=rpart.control(minsplit=5,  # we want to have at least 5 observations in a node
+                                       cp=0.0001,    # we will consider even small improvements in a fit 
+                                       xval=10))
+rpart.plot(big.tree)
+
+nbig = length(unique(big.tree$where))
+cat('size of big tree: ',nbig,'\n')
+
+# let us look at the cross-validation results
+#
+# the following prints out a table summarizing the output of cross-validation
+# you want to find cp corrsponding to the smallest value of xerror 
+(cptable = printcp(big.tree))
+(bestcp = cptable[ which.min(cptable[,"xerror"]), "CP" ])   # this is the optimal cp parameter
+
+## Finds improved xerror
+min(cptable[,"xerror"])
+## Min xerror now .05754176
+
+plotcp(big.tree) # plot results
+
+# show fit from some trees
+oo = order(cars$mileage)
+cpvec = c(.0157,bestcp,.004)
+
+par(mfrow=c(3,2))
+for(i in 1:3) {
+  plot(cars$mileage,cars$price,pch=16,col='blue',cex=.5)
+  ptree = prune(big.tree,cp=cpvec[i])
+  pfit = predict(ptree)
+  lines(cars$mileage[oo],pfit[oo],col='red',lwd=2)
+  title(paste('alpha = ',round(cpvec[i],3)))
+  rpart.plot(ptree)
+}
+
+par(mfrow=c(1,1))
+best.tree = prune(big.tree,cp=bestcp)
+rpart.plot(best.tree)
